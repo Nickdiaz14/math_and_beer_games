cef063074459379cb8790e07099517081448bd9a
Nicolás Díaz Guayazana <elkindiaz1931@gmail.com>
2025-10-28 09:28:17 -0500

    Se realizó la modificación necesaria para tener nuevamente el forms de asistencia

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request, jsonify
+import pytz
+from datetime import datetime
+import psycopg2
+import os
 
 app = Flask(__name__)
 
@@ -13,6 +17,55 @@ def leaderboards():
 @app.route('/menu')
 def about():
     return render_template('menu.html')
+
+@app.route('/forms')
+def forms():
+    return render_template('forms.html')
+
+
+# Conectar a Supabase
+def connect_db():
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port="5432"
+    )
+
+#---------------------------------------------------------attendance---------------------------------------------------------#
+@app.route('/attendance', methods=['POST'])
+def attendance():
+    try:
+        connection = connect_db()
+        cursor = connection.cursor()
+
+        # Procesar los datos
+        nombre_completo = request.form['nombre_completo']
+        sexo = request.form['sexo']
+        edad = request.form['edad']
+        correo_electronico = request.form['correo_electronico']
+        rol = request.form['rol']
+        calificacion = request.form['calificacion']
+        futuros_eventos = request.form['futuros_eventos']
+        comentario = request.form['comentario']
+        timezone = pytz.timezone('America/Bogota')
+        now = datetime.now(timezone)
+        fecha_corta = now.strftime('%Y/%m/%d')
+        fecha_larga = now.strftime('%Y/%m/%d %H:%M')
+        cursor.execute("""
+                INSERT INTO attendance 
+                (created_at, "Fecha", "Nombre", "Sexo", "Edad", "Correo", "Rol", "Calificación", "Futuros_eventos", "Comentario")
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+            """, (fecha_larga, fecha_corta, nombre_completo, sexo, edad, correo_electronico, rol, calificacion, futuros_eventos, comentario))
+
+        connection.commit()
+        cursor.close()
+        connection.close()  
+        return jsonify({'success': True})
+    except Exception as e:
+        print("Error:", e)
+        return jsonify({'success': False})
 
 if __name__ == '__main__':
     app.run(debug=True)
