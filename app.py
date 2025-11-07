0095d671240c960019c2dcada22518b177519535
Nicolás Díaz Guayazana <elkindiaz1931@gmail.com>
2025-11-07 14:24:54 -0500

    Cambio de pregunta sobre rol

--- app.py
+++ app.py
@@ -53,6 +53,7 @@ def attendance():
         futuros_eventos = request.form['futuros_eventos']
         tipo_doc = request.form['tipo_doc']
         numero_doc = request.form['numero_doc']
+        konradista = request.form['konradista']
         comentario = request.form['comentario']
         timezone = pytz.timezone('America/Bogota')
         now = datetime.now(timezone)
@@ -60,9 +61,9 @@ def attendance():
         fecha_larga = now.strftime('%Y/%m/%d %H:%M')
         cursor.execute("""
                 INSERT INTO attendance 
-                (created_at, "Fecha", "Nombre", "Sexo", "Edad", "Correo", "Rol", "Calificación", "Futuros_eventos", "Comentario", "Tipo_documento", "Numero_documento")
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
-            """, (fecha_larga, fecha_corta, nombre_completo, sexo, edad, correo_electronico, rol, calificacion, futuros_eventos, comentario,tipo_doc, numero_doc))
+                (created_at, "Fecha", "Nombre", "Sexo", "Edad", "Correo", "Rol", "Calificación", "Futuros_eventos", "Comentario", "Tipo_documento", "Numero_documento", "Konradista")
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+            """, (fecha_larga, fecha_corta, nombre_completo, sexo, edad, correo_electronico, rol, calificacion, futuros_eventos, comentario,tipo_doc, numero_doc, konradista))
 
         connection.commit()
         cursor.close()
