49a3cdc5b6c634adf36001081a25be302fe7fc88
Nicolás Díaz Guayazana <elkindiaz1931@gmail.com>
2026-05-04 23:14:02 -0500

    Editar yml db.py para github actions

--- db.py
+++ db.py
@@ -10,10 +10,11 @@ _db_pool = None
 def init_sigle_connection():
     return psycopg2.connect(
         host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        port="5432"
+        database=os.getenv("DB_NAME"),
+        port=os.getenv("DB_PORT", "5432"), 
+        sslmode="require" 
     )
 
 def init_db_pool():
